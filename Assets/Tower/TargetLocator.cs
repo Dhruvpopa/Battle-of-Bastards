0b20cfcc559b1e40d8b07720bc0d2dd8c0adce48
Dhruvpopa <202151050@iiitvadodara.ac.in>
2024-06-17 11:36:43 +0530

    Target closest Enemy and null reference error resolved

--- Assets/Tower/TargetLocator.cs
+++ Assets/Tower/TargetLocator.cs
@@ -6,20 +6,59 @@ using UnityEngine;
 public class TargetLocator : MonoBehaviour
 {
     [SerializeField] Transform weapon;
+    [SerializeField] float range = 15f;
+    [SerializeField] ParticleSystem projectileParticles;
     Transform target;
-
-    void Start()
-    {
-        target = FindObjectOfType<EnemyMover>().transform;
-    }
 
     void Update()
     {
         AimWeapon();
+        FindClosestTarget();
+    }
+
+    void FindClosestTarget()
+    {
+        Enemy[] enemies = FindObjectsOfType<Enemy>();
+        Transform closestTarget = null;
+        float maxDistance = Mathf.Infinity;
+
+        foreach (Enemy enemy in enemies) 
+        {
+            float targetDistance = Vector3.Distance(transform.position, enemy.transform.position);
+            if (targetDistance < maxDistance)
+            {
+                closestTarget = enemy.transform;
+                maxDistance = targetDistance;
+            }
+        }
+        target = closestTarget;
     }
 
     void AimWeapon()
     {
-        weapon.LookAt(target);
+        if (target == null)
+        {
+            Attack(false);
+        }
+        else
+        {
+            float targetDistance = Vector3.Distance(transform.position, target.position);
+            weapon.LookAt(target);
+
+            if (targetDistance < range)
+            {
+                Attack(true);
+            }
+            else
+            {
+                Attack(false);
+            }
+        }
+    }
+
+    void Attack(bool isActive)
+    {
+        var emissionModule = projectileParticles.emission;
+        emissionModule.enabled = isActive;
     }
 }
