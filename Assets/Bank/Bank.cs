0a5ccc4f9b0ffb538def49d8bda7ec0df8bcded4
Dhruvpopa <202151050@iiitvadodara.ac.in>
2024-07-17 15:55:24 +0530

    Build WebGL

--- Assets/Bank/Bank.cs
+++ Assets/Bank/Bank.cs
@@ -31,6 +31,10 @@ public class Bank : MonoBehaviour
         {
             ReloadScene();
         }
+        if (currentBalance > 500) 
+        {
+            ReloadScene();
+        }
     }
 
     void UpdateDisplay() 
